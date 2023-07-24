0fae1c07fb57383a4892c742b27f0d12b6b03c7f
Isabela Baseggio <isabela.baseggio7@gmail.com>
2023-07-24 17:28:13 -0300

    arranging components

--- todo/src/components/List.js
+++ todo/src/components/List.js
@@ -1,26 +1,12 @@
 import React from "react";
+import ListItem from "./ListItem";
 
-function DoneImg(props){
-
-    if(props.done){
-        return (<img alt="check done" src="./assets/check_on.svg"></img>)
-    } else {
-        return (<img alt="check undone" src="./assets/check_off.svg"></img>)
-    }
-
-}
-
-function List(props){
-
-    return (<ul>
-        {props.items.map(item => <li className={item.done?"done item":"item"} key={item.id}>
-            {item.text}
-            <button onClick={()=> {props.onDone(item)}}><DoneImg done={item.done}></DoneImg></button>
-            <button onClick={()=>{props.onItemDelete(item)}}>
-                <img src="./assets/bin.png" alt="delete"></img>
-            </button>
-        </li>)}
-    </ul>);
+function List(props) {
+  return (
+    <ul>
+      {props.items.map(item => <ListItem key={item.id} item={item} onDone={props.onDone} onItemDelete={props.onItemDelete}></ListItem>)}
+    </ul>
+  );
 }
 
 export default List;
---
+++ todo/src/components/ListItem.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Card from "./Card";
+
+function DoneImg(props) {
+  if (props.done) {
+    return <img alt="check done" src="./assets/check_on.svg"></img>;
+  } else {
+    return <img alt="check undone" src="./assets/check_off.svg"></img>;
+  }
+}
+
+function ListItem(props) {
+  return (<li>
+          <Card className={props.item.done ? "done item" : "item"}>
+            {props.item.text}
+            <div>
+              <button
+                onClick={() => {
+                  props.onDone(props.item);
+                }}
+              >
+                <DoneImg className="img" done={props.item.done}></DoneImg>
+              </button>
+              <button
+                onClick={() => {
+                  props.onItemDelete(props.item);
+                }}
+              >
+                <img className="img" src="./assets/bin.png" alt="delete"></img>
+              </button>
+            </div>
+          </Card>
+        </li>
+  );
+}
+
+export default ListItem;
